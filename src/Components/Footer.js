ff1069af820f370428c627ec1d73245ad95dbdc5
Darren Willoughby <darrenwilloughby@adviserplus.com>
2021-03-15 08:33:51 +0000

    Replace ahref with Link. Added useLocation to display add button only on a certain route.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,9 @@
-
+import { Link } from 'react-router-dom'
 const Footer = () => {
     return (
         <footer className='footer'>
             <p>Copyright &copy; 2021</p>
-            <a href='/about'>About</a>
+            <Link to='/about'>About</Link>
         </footer>
     )
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,20 @@
+import { useLocation } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = ({ title, onAdd, showAddTask }) => {
+
+    const location = useLocation()
+
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button
-                onClick={onAdd}
-                color={showAddTask ? 'red' : 'green'}
-                text={showAddTask ? 'hide' : 'new'}
-            />
+            {location.pathname === '/' &&
+                <Button
+                    onClick={onAdd}
+                    color={showAddTask ? 'red' : 'green'}
+                    text={showAddTask ? 'hide' : 'new'}
+                />}
         </header>
     )
 }
